4ad0caf6e6f75a337c2c06574101e07748372ac1
nakhun9279 <fjfj9279@naver.com>
2020-09-01 19:24:08 +0900

    17.5.1-1 컨테이너 만들기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Counter from './components/Counter';
 import Todos from './components/Todos';
+import CounterContainer from './containers/CounterContainer';
+
 
 const App = () => {
   return (
     <div>
-      <Counter number={0} />
+      <CounterContainer />
       <hr />
       <Todos />
     </div>
   );
 };
+
+
 
 export default App;
