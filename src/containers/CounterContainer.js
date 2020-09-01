83e53493e257532d94dec160bf1cdc4450581af4
nakhun9279 <fjfj9279@naver.com>
2020-09-01 19:55:14 +0900

    17.7.1 useSelector로 상태 조회하기

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return (
-      <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-    );
-  };
-  
-  
-  
-  export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-      increase,
-      decrease,
-    },
-  )(CounterContainer);
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  return <Counter number={number} />;
+};
+
+export default CounterContainer;
