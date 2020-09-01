bd704f8ef7fa31fd3b27bcd8e4a1da594e4d6993
nakhun9279 <fjfj9279@naver.com>
2020-09-01 19:19:24 +0900

    17.4.3 Redux Dev Tools설치 및 적용

--- src/modules/index.js
+++ src/modules/index.js
@@ -8,15 +8,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
 
-
 const store = createStore(rootReducer, composeWithDevTools());
 
-
-
-ReactDOM.render( <Provider store={store}>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </Provider>, document.getElementById('root'));
-
-
+  </Provider>,
+  document.getElementById('root'),
+);
 
 serviceWorker.unregister();
